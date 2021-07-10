366c709d5e314d51b6fe45e14b22bcaa13ac850c
koda31 <adamsmith21@verizon.net>
2021-07-10 14:40:11 +0100

    create the draw_board function

--- app.py
+++
@@ -1,7 +0,0 @@
-from blessed import Terminal
-
-if __name__ == "__main__":
-    term = Terminal()
-
-    print(term.home + term.clear + term.move_y(term.height // 2))
-    print(term.black_on_darkkhaki(term.center("Hello World")))

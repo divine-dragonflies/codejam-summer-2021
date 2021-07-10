0347187934e425e8a31a2067fff5ba7d6e329e7b
koda31 <adamsmith21@verizon.net>
2021-07-10 12:57:11 +0100

    fix for windows lints

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from blessed import Terminal
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     term = Terminal()
 
     print(term.home + term.clear + term.move_y(term.height // 2))
-    print(term.black_on_darkkhaki(term.center('Hello World')))
+    print(term.black_on_darkkhaki(term.center("Hello World")))

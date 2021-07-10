12c8cee04781f5a5e430ca8e5629300a53c0b7f0
Egor Shatrov <xutpuu@gmail.com>
2021-07-10 16:10:01 +0300

    formating was fixed

--- src/main_loop.py
+++ src/main_loop.py
@@ -2,14 +2,18 @@ import blessed
 
 term = blessed.Terminal()
 
+
 def left_of():
     print("Left was passed")
+
 
 def right_of():
     print("Right was passed")
 
+
 def above():
     print("Above was passed")
+
 
 def below():
     print("Below was passed")
@@ -17,21 +21,22 @@ def below():
 
 def key_mapping(key):
     functions = {
-        'a': left_of,
-        'd': right_of,
-        'w': above,
-        's': below,
+        "a": left_of,
+        "d": right_of,
+        "w": above,
+        "s": below,
     }
     func = functions[key]
     func()
 
+
 def main_loop():
     with term.cbreak():
         val = term.inkey()
-        #It will be working until ESCAPE pressed
-        while val.code != term.KEY_ESCAPE: 
+        # It will be working until ESCAPE pressed
+        while val.code != term.KEY_ESCAPE:
             val = term.inkey()
-            if val and str(val) in 'wasd':
+            if val and str(val) in "wasd":
                 key_mapping(str(val))
             else:
                 pass
